ee772c6b0c121c93b908f3acc71071db88b6cf4c
budRich <robstenklippa@gmail.com>
2025-07-14 00:58:40 +0200

    ORG , created src and data dir
    Also added xdg.c/h from halen , they are currently not in use.
    Updated makefile to respect these new directories, and also install default config.
    
    Also in makefile, we now create build_dir/config.h ... ah, that make me realize we should
    rename config.c config.h in src directory...

---
+++ src/xdg.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <linux/limits.h>
+
+typedef enum {
+    XDG_RUNTIME_DIR,
+    XDG_CONFIG_HOME,
+    XDG_CACHE_HOME,
+    XDG_DATA_HOME
+} xdg_directory_type_t;
+
+char* xdg_get_directory(xdg_directory_type_t directory_type);
+char* xdg_get_user_config_path(const char *program_name);
